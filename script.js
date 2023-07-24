0a6970e5bc5200657d0bd5f6048f96663e63ac95
Mike Weatherford <mikesweatherford@gmail.com>
2023-07-24 19:52:52 -0400

    added address, fixed font, tuned button animation, added review button

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const toggleButton = document.getElementById('toggle-button');
 const naviList = document.getElementById('navi-list');
 
+
 toggleButton.addEventListener('click', () => {
     naviList.classList.toggle('active');
 });
+
+const reviewButton = document.getElementById('see-review');
+
+reviewButton.addEventListener('click', () => {
+
+    const review = document.getElementById('review');
+
+    if (review.classList.contains('d-none')) {
+        review.classList.remove('d-none');
+    } else {
+        review.classList.add('d-none');
+    }
+});
+
